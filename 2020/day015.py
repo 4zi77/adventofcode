ba7c8819f4b6eec1ed1213274575e3fa92beb409
Esteban Gougeon <esteban@enlaps.fr>
2020-12-15 12:28:05 +0100

    PEP8 day15

--- 2020/day015.py
+++ 2020/day015.py
@@ -2,24 +2,25 @@ import time
 import statistics as st
 
 test_dict = {
-    "test_1" : [0,3,6],  # 436 175594
-    "test_2" : [1,3,2],  # 1 2578
-    "test_3" : [2,1,3],  # 10 3544142
-    "test_4" : [1,2,3],  # 27 261214
-    "test_5" : [2,3,1],  # 78 6895259
-    "test_6" : [3,2,1],  # 438 18
-    "test_7" : [3,1,2],  # 1836 362
+    "test_1": [0, 3, 6],  # 436 175594
+    "test_2": [1, 3, 2],  # 1 2578
+    "test_3": [2, 1, 3],  # 10 3544142
+    "test_4": [1, 2, 3],  # 27 261214
+    "test_5": [2, 3, 1],  # 78 6895259
+    "test_6": [3, 2, 1],  # 438 18
+    "test_7": [3, 1, 2],  # 1836 362
 }
-real = [14,1,17,0,3,20]
+real = [14, 1, 17, 0, 3, 20]
 iteration = 2020
 iteration2 = 30000000
 
-def predict_ith(L, it, it2):
+
+def predict_ith(numbers, it, it2):
     occurences = {}
-    for i, v in enumerate(L):
+    for i, v in enumerate(numbers):
         occurences[v] = i+1
     last_val = 0
-    for i in range(len(L)+1, it2):
+    for i in range(len(numbers)+1, it2):
         if last_val in occurences:
             new_val = i - occurences[last_val]
             occurences[last_val] = i
@@ -32,11 +33,12 @@ def predict_ith(L, it, it2):
             print(f"Value = {last_val}")
     return last_val
 
+
 if __name__ == '__main__':
     times = []
-    for i in range(1, 8):
+    for j in range(1, 8):
         start = time.time()
-        number_list = test_dict[f"test_{i}"]
+        number_list = test_dict[f"test_{j}"]
         print(predict_ith(number_list, iteration, iteration2), '\n')
         times.append(time.time()-start)
     start = time.time()
